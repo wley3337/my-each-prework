4c8b22a84bbea8e703daa4618c0168815bc92760
wley3337@gmail.com <wley3337@gmail.com>
2018-05-06 23:58:48 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,7 @@
 def my_each(words) 
  i = 0 
  while i < words.length # so this is making sure you go through the block enough times that you'll hit every element.
-   yield (words [i]) # so this is saying output the array with index of i which is the iteration that the #.each does
+   yield (words [i]) # so this is saying output the array with index of i which is the iteration that the #.each does, yield also acts as return elment without interupting the loop itself
    i += 1
  end
  words #this is returning the original array which is the return of the #.each 
